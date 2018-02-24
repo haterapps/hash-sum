d83e58dff15c116f7ca87a97fcc815db7b3d6dbe
Hater Dev <frustrations@haterapps.com>
2018-02-24 10:58:05 +0200

    strip whitespaces and new lines

--- hash-sum.js
+++ hash-sum.js
@@ -4,6 +4,7 @@ var MD5 = require('md5.js')
 
 function hash(str) {
   str = str.replace(/\\/g, '/'); // this is required because Windows is creating paths with "\" vs "/" like all other systems, and will produce a different md5 hash
+  str = str.replace(/(\r\n|\n|\r|\s)/gm,""); // same as above, but for whitespaces and new lines
   var md5hash = new MD5().update(str).digest('hex');
 
   return 'HA_MODULE_'+md5hash;
