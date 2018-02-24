3a638a4e6a3f4cd8652efeb92a4a9017823e0383
Hater Dev <frustrations@haterapps.com>
2018-02-24 02:37:09 +0200

    added readme file

--- hash-sum.js
+++ hash-sum.js
@@ -3,10 +3,9 @@
 var MD5 = require('md5.js')
 
 function hash(str) {
-  str = str.replace(/\\/g, '/');
+  str = str.replace(/\\/g, '/'); // this is required because Windows is creating paths with "\" vs "/" like all other systems, and will produce a different md5 hash
   var md5hash = new MD5().update(str).digest('hex');
 
-  console.log(md5hash);
   return 'HA_MODULE_'+md5hash;
 }
 
